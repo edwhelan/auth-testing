b3592d26e0e250e1cb096eacc08a6697d59a46e4
Edward Whelan <vroxing@gmail.com>
2018-12-25 08:41:07 -0500

    add saved info

---
+++ src/index.js
@@ -0,0 +1,74 @@
+// import dependencies 
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+//define the express app for use
+const app = express();
+
+// basic database for now
+const question = [];
+
+//security for header functionality 
+app.use(helmet());
+
+// body parser to parse application JSON content-type
+app.use(bodyParser.json())
+
+// enable all CORS requests
+app.use(cors());
+
+//log http requests
+app.use(morgan('combined'));
+
+//root
+app.get('/', (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length,
+  }));
+  res.send(qs);
+});
+
+//get a specific question
+app.get('/:id', (req, res) => {
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  res.send(question[0]);
+});
+
+app.post('/', (req, res) => {
+  const { title, description } = req.body;
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: [],
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+//insert a new answer to a question
+app.post('/answer/:id', (req, res) => {
+  const { answer } = req.body;
+
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (questions.length === 0) return res.status(404).send();
+
+  question[0].answers.push({
+    answer,
+  });
+
+  res.status(200).send();
+})
+
+app.listen(5000, () => {
+  console.log('listening on port 5000')
+})
