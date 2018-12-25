8e00e711a39119b9d57a1bb82eb0b320225d8a82
Edward Whelan <vroxing@gmail.com>
2018-12-25 09:32:05 -0500

    add question component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
+import Questions from './components/Questions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Questions />
         <p>work in progress...</p>
       </div>
     );
---
+++ client/src/components/Questions.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Questions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: null,
+    };
+  }
+  render() {
+    return (
+      <div>hello</div>
+    )
+  }
+}
+
+export default Questions;
